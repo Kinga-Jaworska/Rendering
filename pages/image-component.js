35e5d46390e49dc19ced0f825e7fb825674ffe4a
Kinga Jaworska <artiming5@gmail.com>
2022-11-23 22:45:53 +0100

    cat

--- pages/image-component.js
+++ pages/image-component.js
@@ -1,12 +1,15 @@
+import Image from "next/image";
+
 export const ImageContent = (props) => {
   return (
     <>
       <h1>{props.title}</h1>
       <h3>CAT FACTS</h3>
       <div className="image">
-        <img
+        <Image
           src="https://movetexas.org/wp-content/uploads/2021/03/IMG_1974.jpg"
           height={80}
+          alt="Cat okay meme"
         />
       </div>
     </>
